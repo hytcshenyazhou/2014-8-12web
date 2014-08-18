2b4c17ae8b2cc21e2db8f104b467c129fba81ee6
hytcshenyazhou <641077955@qq.com>
2014-08-18 16:40:59 +0800

    Revert "努力奋斗  吼吼吼"
    This reverts commit b98cf424667dc1922a53588e26e3eb9cd3532463.

--- WEB QQ/js/WebQQ.js
+++ WEB QQ/js/WebQQ.js
@@ -8,50 +8,13 @@ $(function(){
 		 
 	});
 
+	$(function() {
+	$( "#List" ).sortable();
+	$( "#List" ).disableSelection();
+	});
 	$(".BTW").click(function(){
-		var b=$(this).attr("bn");
 		$(".BTW").removeClass("active");
 		$(this).addClass("active");
-		$(".Y").hide();
-		$(".nei"+b).show();
-	});
-
-	$(".L1").click(function(){
-		var c=$(this).attr("bto");
-		var d=$(this).attr("BB");
-		if (d==1) {
-			$(".F"+c).show();
-			$(".L1").attr("BB",0);
-		}
-		else{
-			$(".F"+c).hide();
-			$(".L1").attr("BB",1);
-		};		
-	});
-	$(".Button1").click(function(){
-		var e=$(this).attr("Button-s");
-		if (e==1) {
-			$(".In1-a").slideDown();
-			$(this).attr("Button-s",0);
-		}
-		else{
-			$(".In1-a").slideUp();
-			$(this).attr("Button-s",1);
-		};
-	});
-	$(".close").click(function(){
-		$("#container").hide();
-	});
-	$(".sanhang").click(function(){
-		var f=$(this).attr("bt");
-		if (f==1) {
-			$(".sanhang").show();
-			$(this).attr("bt",0);
-		}else
-		{
-			$(".sanhang").hide();
-			$(this).attr("bt",1);
-		};
 	});
 });
 
